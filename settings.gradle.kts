29c35768e953639da3eeb07ea192078d0714349e
Lastlilith <lastlilith.2014@gmail.com>
2023-01-25 22:04:12 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "com.imnidasoftware.googleauthserver"
---
+++ src/main/kotlin/com/imnidasoftware/Application.kt
@@ -0,0 +1,14 @@
+package com.imnidasoftware
+
+import com.imnidasoftware.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
