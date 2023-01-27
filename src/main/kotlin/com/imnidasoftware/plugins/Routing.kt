dfa83b0b0c5c3465640b5e48889d85b98bac1291
Lastlilith <lastlilith.2014@gmail.com>
2023-01-27 23:55:38 +0900

    token id verification

--- src/main/kotlin/com/imnidasoftware/plugins/Routing.kt
+++ src/main/kotlin/com/imnidasoftware/plugins/Routing.kt
@@ -10,7 +10,7 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         rootRoute()
-        tokenVerificationRoute()
+        tokenVerificationRoute(application)
         authorizedRoute()
         unauthorizedRoute()
     }
---
+++ src/main/kotlin/com/imnidasoftware/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.imnidasoftware.util
+
+object Constants {
+
+    const val AUDIENCE = "132893197858-9btt6lcu4gol9aogdl5nfev44m2249ic.apps.googleusercontent.com"
+    const val ISSUER = "https://accounts.google.com"
+
+    const val DATABASE_NAME = "user_database"
+}
