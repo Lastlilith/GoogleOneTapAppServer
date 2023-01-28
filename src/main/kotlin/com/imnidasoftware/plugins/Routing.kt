2349bd25a37615a97e905bf37aa61c0709a1569f
Lastlilith <lastlilith.2014@gmail.com>
2023-01-28 14:49:32 +0900

    implemented GetUserInfo route

--- src/main/kotlin/com/imnidasoftware/plugins/Routing.kt
+++ src/main/kotlin/com/imnidasoftware/plugins/Routing.kt
@@ -1,10 +1,7 @@
 package com.imnidasoftware.plugins
 
 import com.imnidasoftware.domain.repository.UserDataSource
-import com.imnidasoftware.routes.authorizedRoute
-import com.imnidasoftware.routes.rootRoute
-import com.imnidasoftware.routes.tokenVerificationRoute
-import com.imnidasoftware.routes.unauthorizedRoute
+import com.imnidasoftware.routes.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import org.koin.java.KoinJavaComponent.inject
@@ -15,6 +12,7 @@ fun Application.configureRouting() {
 
         rootRoute()
         tokenVerificationRoute(application, userDataSource)
+        getUserInfoRoute(application, userDataSource)
         authorizedRoute()
         unauthorizedRoute()
     }
